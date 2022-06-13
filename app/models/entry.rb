ea9e0424644404967f3539215160a3486491e8b5
biancapower <bianca@biancapower.com>
2022-06-13 20:27:57 +1000

    add entry_type enum to entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,14 @@
 class Entry < ApplicationRecord
   belongs_to :collection
   has_rich_text :body
+
+  enum entry_type: {
+    task_incomplete: 1,
+    task_completed: 2,
+    task_forwarded: 3,
+    task_dismissed: 4,
+    event: 5,
+    note: 6,
+    idea: 7
+  }
 end
